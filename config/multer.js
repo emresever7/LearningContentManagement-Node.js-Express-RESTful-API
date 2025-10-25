7934538a2731155ddd7c12c6fad0adb31d0337dd
Emre Sever <emresever7@outlook.com>
2025-10-25 19:16:00 +0300

    Add AWS S3 support

--- config/multer.js
+++ config/multer.js
@@ -1,24 +1,33 @@
+const { S3Client } = require('@aws-sdk/client-s3');
 const multer = require('multer');
-const path = require('path');
+const multerS3 = require('multer-s3');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '..', '..', 'public', 'uploads')); 
+// AWS S3 yapılandırması
+const s3 = new S3Client({
+  credentials: { 
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-  },
+  region: process.env.AWS_REGION 
 });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/') || file.mimetype === 'application/pdf') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
 
-const upload = multer({ storage: storage, fileFilter: fileFilter });
+const s3Storage = multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    metadata: function (req, file, cb) {
+        cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+        const fileName = `${file.fieldname}-${Date.now()}-${file.originalname}`;
+        cb(null, fileName); 
+    }
+});
+
+
+const upload = multer({ 
+    storage: s3Storage,
+    limits: { fileSize: 1024 * 1024 * 10 }
+});
 
 module.exports = upload;
